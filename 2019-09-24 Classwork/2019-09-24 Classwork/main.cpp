8e04d264db6a0fc66fa6f283f84a569345d923a1
Richiemilliemilty <55443426+Richiemilliemilty@users.noreply.github.com>
2019-09-24 08:44:21 -0400

    Add files via upload

---
+++ 2019-09-24 Classwork/2019-09-24 Classwork/main.cpp
@@ -0,0 +1,18 @@
+// Name: MIlton Allen
+// Intro to computer science
+//Period 1 
+// 2019-09-24 Classwork
+
+#include <iostream>
+
+int main()
+{
+	int x = 5;
+	int y = 8;
+	float x = 1000;
+	int n = 123;
+	int z = 2000;
+	
+	std::cout << sizeof(x) << std::endl;
+	x = 5;
+}
