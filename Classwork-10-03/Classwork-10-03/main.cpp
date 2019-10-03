3203468f4a20762c817b6cde046030fcf46355d0
Richiemilliemilty <55443426+Richiemilliemilty@users.noreply.github.com>
2019-10-03 08:41:09 -0400

    Add files via upload

---
+++ Classwork-10-03/Classwork-10-03/main.cpp
@@ -0,0 +1,18 @@
+// Milton Allen 
+// Intro to CS
+// Period 1
+// Mr.williams 
+// 2019-10-03
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "number?";
+	int x;
+	cin >> x;
+	cout << "cool" << x << "is my favorite number too!" << endl;
+
+
+}
